aa8d0e3a1737dee5c88c61b7d1dfadbab680f4b5
Jerome Bakker <jeabakker@coldtrick.com>
2015-03-26 11:10:05 +0100

    dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,14 @@
+<?php
+$language = array (
+  'tell_a_friend:share' => 'Tip een vriend',
+  'tell_a_friend:share_title' => 'Tip een vriend',
+  'tell_a_friend:share:recipient' => 'Ontvanger(s)',
+  'tell_a_friend:share:subject' => 'Onderwerp',
+  'tell_a_friend:share:message' => 'Bericht',
+  'tell_a_friend:share:required' => 'Velden gemarkeerd met een * zijn verplicht',
+  'tell_a_friend:action:share:error:recipients' => 'Geef ten minste 1 ontvanger op',
+  'tell_a_friend:action:share:error:subject' => 'Geef een onderwerp op',
+  'tell_a_friend:action:share:error:message' => 'Geef een bericht op',
+  'tell_a_friend:action:share:success' => 'Je bericht is verstuurd',
+);
+add_translation("nl", $language);
