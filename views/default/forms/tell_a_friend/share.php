b7919bba2dc48175a7ac739b0cbaadc9be955558
Jerome Bakker <jeabakker@coldtrick.com>
2019-03-13 11:32:48 +0100

    chore: reworked form

--- views/default/forms/tell_a_friend/share.php
+++ views/default/forms/tell_a_friend/share.php
@@ -1,46 +1,49 @@
 <?php
 
-$entity = elgg_extract("entity", $vars);
-if (empty($entity) || !elgg_instanceof($entity)) {
+$entity = elgg_extract('entity', $vars);
+if (!$entity instanceof ElggEntity) {
 	return;
 }
 
 $user = elgg_get_logged_in_user_entity();
 
-$subject = elgg_echo("tell_a_friend:share:subject:default", array($user->name));
+$subject = elgg_echo('tell_a_friend:share:subject:default', [
+	$user->getDisplayName(),
+]);
 
-$title = $entity->title;
-if (empty($title)) {
-	$title = $entity->name;
-}
-$message = elgg_echo("tell_a_friend:share:message:default", array(
-	$user->name,
-	$title,
-	$entity->getURL()
-));
+$message = elgg_echo('tell_a_friend:share:message:default', [
+	$user->getDisplayName(),
+	$entity->getDisplayName(),
+	$entity->getURL(),
+]);
 
-echo "<div>";
-echo "<label>" . elgg_echo("tell_a_friend:share:recipient") . "*</label>";
-echo elgg_view("input/userpicker");
-echo "<div class='ui-front'></div>";
-echo "</div>";
+echo elgg_view_field([
+	'#type' => 'userpicker',
+	'#label' => elgg_echo('tell_a_friend:share:recipient'),
+	'name' => 'members',
+	'required' => true,
+]);
 
-echo "<div>";
-echo "<label for='tell-a-friend-share-subject'>" . elgg_echo("tell_a_friend:share:subject") . "*</label>";
-echo elgg_view("input/text", array("id" => "tell-a-friend-share-subject", "name" => "subject", "value" => $subject));
-echo "</div>";
+echo elgg_view_field([
+	'#type' => 'text',
+	'#label' => elgg_echo('tell_a_friend:share:subject'),
+	'name' => 'subject',
+	'value' => $subject,
+	'required' => true,
+]);
 
-echo "<div>";
-echo "<label for='tell-a-friend-share-message'>" . elgg_echo("tell_a_friend:share:message") . "*</label>";
-echo elgg_view("input/plaintext", array("id" => "tell-a-friend-share-message", "name" => "message", "value" => $message));
-echo "</div>";
+echo elgg_view_field([
+	'#type' => 'plaintext',
+	'#label' => elgg_echo('tell_a_friend:share:message'),
+	'name' => 'message',
+	'value' => $message,
+	'required' => true,
+]);
 
-echo "<div class='elgg-subtext'>" . elgg_echo("tell_a_friend:share:required") . "</div>";
+// form footer
+$footer = elgg_view_field([
+	'#type' => 'submit',
+	'value' => elgg_echo('send'),
+]);
 
-echo "<div class='elgg-foot'>";
-echo elgg_view("input/submit", array("value" => elgg_echo("send")));
-echo "</div>";
-?>
-<script>
-elgg.userpicker.init();
-</script>
+elgg_set_form_footer($footer);
